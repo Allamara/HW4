f2a56b8620851a0d17ed3c8ccb7913d225e02967
amakhanova <annanen@inbox.ru>
2023-10-12 00:03:34 +0300

    Test

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,46 @@
 public class Main {
     public static void main(String[] args) {
-        int age = 17;
+// TASK_1
+        int age = 15;
         if (age >= 18) {
-            System.out.println("Ты можешь водить");
+            System.out.println("Если возраст человека равен " + age + ", то он совершеннолетний");
+        }
+        else {
+            System.out.println("Если возраст человека равен " + age + ", то он не достиг совершеннолетия," +
+                    " нужно немного подождать");
         }
 
-        if (age < 18) {
-            System.out.println("Ты не можешь водить");
+// TASK_2
+        int temp = 20;
+        if (temp <= 5) {
+            System.out.println("На улице " + temp + " градусов, нужно надеть шапку");
+        }
+        else {
+            System.out.println("На улице " + temp + " градусов, можно идти без шапки");
         }
 
+// TASK_3
+        int speed = 150;
+        if (speed <= 60) {
+            System.out.println("Если скорость " + speed + " км/ч, то можно ездить спокойно");
+        }
+        else {
+            System.out.println("Если скорость " + speed + " км/ч, то придётся заплатить штраф");
+        }
+
+// TASK_4
+        int personAge = 20;
+        if (personAge >= 2 && personAge <= 6) {
+            System.out.println("Если возраст человека равен " + personAge + " ,то ему нужно ходить в детский сад");
+        }
+        if (personAge >= 7 && personAge <= 17) {
+            System.out.println("Если возраст человека равен " + personAge + " ,то ему нужно ходить в школу");
+        }
+        if (personAge >= 18 && personAge <= 24) {
+            System.out.println("Если возраст человека равен " + personAge + " ,то ему нужно ходить в университет");
+        }
+        if (personAge >= 24) {
+            System.out.println("Если возраст человека равен " + personAge + " ,то ему нужно ходить на работу");
+        }
     }
 }
